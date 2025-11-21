45840191cdd2b82287990f5140d0779f3291a610
nikita <nikilodion87@gmail.com>
2025-11-21 16:12:28 +0300

    settings

--- myshop/settings.py
+++ myshop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'catalog',
+    'blogs',
 ]
 
 MIDDLEWARE = [
@@ -125,7 +126,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-INSTALLED_APPS = [
-    'blogs',
-]
